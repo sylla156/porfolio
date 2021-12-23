7560f5b57e141248f0709a916652f3527e0b36f0
sylla ibrahim <s67986601@gmail.com>
2021-12-23 07:01:20 +0000

    deuxieme

---
+++ src/js/conpoment/Project.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Project extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <main className="project">
+        <h1 className="project__title">my project</h1>
+        <div className="project__first" >
+          <h2 className="project__first--title">e-commerce</h2>
+          <div className="project__first--desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis fugit autem minus. Est sapiente corrupti, veritatis
+            ducimus nihil commodi nobis unde! Impedit quaerat ut autem, possimus
+            labore dolorum! Nihil, pariatur?
+          </div>
+
+          <div className="project__first--img">
+          </div>
+        
+        </div>
+      </main>
+    );
+  }
+}
